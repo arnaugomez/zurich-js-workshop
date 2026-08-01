657c216d17a46bd063bf57f25f66d6d71df2cf52
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2026-08-01 11:16:10 +0200

    feat: find-mentioned-items exercise

--- exercises/find-mentioned-items/src/exercise/findMentionedItems.ts
+++ exercises/find-mentioned-items/src/exercise/findMentionedItems.ts
@@ -1,9 +1,9 @@
-import type { JSONContent } from '@tiptap/core'
-import { classroomNotes, type ClassroomNote } from '../data/classroom-notes'
+import type { JSONContent } from "@tiptap/core";
+import { classroomNotes, type ClassroomNote } from "../data/classroom-notes";
 
 export function findMentionedItems(document: JSONContent): ClassroomNote[] {
-  console.log(classroomNotes)
-  // TODO: Walk through the Tiptap JSON document and return the unique
-  // classroom notes referenced by Mention nodes.
-  throw new Error('Not implemented')
+  console.log(classroomNotes);
+  // TODO: Walk through the Tiptap JSON document and return a list of all the
+  // classroom notes referenced by Mention nodes, without duplicates.
+  throw new Error("Not implemented");
 }
